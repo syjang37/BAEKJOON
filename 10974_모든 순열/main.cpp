6d7ead48b6882e9abe1bee4c9064ed4b515fef4a
syjang37 <slong94@naver.com>
2020-08-21 03:16:05 +0900

    10974_모든 순열

--- 10974_모든 순열/main.cpp
+++ 10974_모든 순열/main.cpp
@@ -3,16 +3,12 @@
 #include <algorithm>	
 using namespace std;
 
-int binomial(int n, int k);
 int main()
 {
-	// Get Number.
-	long long n = 0;
-	long long k = 0;
-	scanf("%d %d", &iNumber);
+	int n = 0;
+	scanf("%d", &n);
 	fflush(stdin);
 	
-	// Init.
 	vector<int> vPermutation(iNumber);
 	for (int i=0; i<iNumber; i++) {
 		vPermutation[i] = i + 1;
@@ -26,12 +22,4 @@ int main()
 	} while ( next_permutation(vPermutation.begin(), vPermutation.end()) );
 
 	return 0;
-}
-
-int binomial(int n, int k) {
-	static long long
-	if (n == k || k == 0) {
-		return 1;	
-	}
-	return binomial(n - 1, k) + bionmial(n - 1, k - 1);
 }
