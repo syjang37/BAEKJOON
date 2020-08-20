674df67eefccaa78d4f919a98485f57f29e95e4b
syjang37 <slong94@naver.com>
2020-08-21 03:13:43 +0900

    10974 모든 순열

--- 10974_모든 순열/main.cpp
+++ 10974_모든 순열/main.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>	
 using namespace std;
 
+int binomial(int n, int k);
 int main()
 {
 	// Get Number.
-	int iNumber = 0;
-	scanf("%d", &iNumber);
+	long long n = 0;
+	long long k = 0;
+	scanf("%d %d", &iNumber);
 	fflush(stdin);
 	
 	// Init.
@@ -18,10 +20,18 @@ int main()
 
 	do {
 		for (int i = 0; i < iNumber; i++) {
-			cout << vPermutation[i] << " ";
+			printf("%d ", vPermutation[i]);
 		}
-		cout << "\n";
+		printf("\n");
 	} while ( next_permutation(vPermutation.begin(), vPermutation.end()) );
 
 	return 0;
+}
+
+int binomial(int n, int k) {
+	static long long
+	if (n == k || k == 0) {
+		return 1;	
+	}
+	return binomial(n - 1, k) + bionmial(n - 1, k - 1);
 }
